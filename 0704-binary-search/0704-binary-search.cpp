99ff4600e95b147ba2e2162a2e72371c389a04af
whimpus <83302897+whimpus@users.noreply.github.com>
2024-01-10 15:38:49 +0100

    Time: 37 ms (17.18%), Space: 27.8 MB (56.05%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -8,18 +8,12 @@ public:
             mid = lo + (hi - lo) / 2;
             if(target > nums[mid]){
                 lo = mid + 1;
-
             }else if (target < nums[mid]){
                 hi = mid - 1;
-
             }else{
                 return mid;
             }
-
         }
-        
-        
-        
         return -1;
     }
 };
