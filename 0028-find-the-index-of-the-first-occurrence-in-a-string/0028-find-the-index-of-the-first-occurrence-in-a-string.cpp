ae6994415973150a7f9beeac2e6c57753d2d464e
whimpus <83302897+whimpus@users.noreply.github.com>
2024-01-13 17:02:38 +0100

    Time: 4 ms (8.75%), Space: 6.6 MB (54.49%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n=haystack.size();
-        int m=needle.size();
-        for(int i=0;i<n;i++){
-            if(haystack[i]==needle[0]){
-                bool flag=true;
-                for(int k=0;k<m;k++){
-                    if(needle[k]!=haystack[i+k]){
-                        flag=false;
-                        break;
-                    }
-                }
-                if(flag)
-                return i;
+        int m = 10000007;
+        int base = 26;
+        int hash = 0;
+        int hash2 = 0;
+        
+        for(char x : needle){
+            hash = (hash%m * base%m)%m;
+            hash += (x - 'a' + 1);
+            hash %= m;
+        }
+        int pw = 1;
+        for(int i  = 0; i < haystack.size(); i++){
+            char ch = haystack[i];
+            
+            hash2 = (hash2%m * base%m)%m;
+            hash2 += (ch - 'a' + 1);
+            hash2 %= m;
+            cout<<"HASH1: "<<hash<<" HASH2 : "<<hash2<<endl;
+            if(i >= needle.size()){ 
+                hash2 = hash2 - ((haystack[i - needle.size()] - 'a'+ 1 )%m * pw%m)%m;
+                hash2 = (hash2 + m)%m;
+            }else{
+                pw = (pw%m * base%m)%m;
+            }
+            cout<<"HASH1: "<<hash<<" HASH2 : "<<hash2<<endl;
+            if(hash2 == hash){
+                return i - needle.size() + 1;
             }
         }
+        
         return -1;
     }
 };
