5affc3e021c71bc5dbf09d166a5b78d2b5dbbd4b
whimpus <83302897+whimpus@users.noreply.github.com>
2024-01-13 19:31:44 +0100

    Time: 3 ms (45.62%), Space: 6.6 MB (54.49%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -14,18 +14,16 @@ public:
         int pw = 1;
         for(int i  = 0; i < haystack.size(); i++){
             char ch = haystack[i];
-            
             hash2 = (hash2%m * base%m)%m;
             hash2 += (ch - 'a' + 1);
             hash2 %= m;
-            cout<<"HASH1: "<<hash<<" HASH2 : "<<hash2<<endl;
+
             if(i >= needle.size()){ 
                 hash2 = hash2 - ((haystack[i - needle.size()] - 'a'+ 1 )%m * pw%m)%m;
                 hash2 = (hash2 + m)%m;
             }else{
                 pw = (pw%m * base%m)%m;
             }
-            cout<<"HASH1: "<<hash<<" HASH2 : "<<hash2<<endl;
             if(hash2 == hash){
                 return i - needle.size() + 1;
             }
